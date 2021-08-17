f310cdc57e3d0205905926d8d9b68297ff52028d
Jay Sanders <jaysanders@Jays-MacBook-Pro.local>
2021-08-16 21:32:13 -0700

    small change

--- api-service/src/index.ts
+++ api-service/src/index.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 
 const start = async () => {
     app.listen(3000, () => {
-        console.log('listening to Port 3000');
+        console.log('listening to Port 3000!');
     });
 };
 
